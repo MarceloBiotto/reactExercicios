790a8b99d5d6bb49854ed6cb41f06397a1fe525a
Marcelo Biotto <lee.am@live.com>
2023-10-25 06:42:53 -0300

    adicionando repositorio de estudos

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+import './App.css';
+import GatoGordo from './assets/obesidade-gatos.jpg'
+import ListRender from './components/ListRender';
+import ManageData from './components/ManageData';
+import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
+import { useState } from 'react';
+import CarDetails from './components/CarDetails';
+
+
+function App() {
+
+  const name = "Joaquim";
+  const[userName] = useState("Maria");
+  return (
+    <div className="App">
+      <h1>
+        Isso é o desafio3!
+        </h1>
+   {/* Imagem em public */}
+        <div>
+          <img src="/gato-gordo-o-que-fazer-para-ajudar-1.jpg" alt="gato gordo" />
+        </div>
+
+        {/*imagem em asset*/}
+        <div>
+          <img src={GatoGordo} alt="obesidade entre gatos" />
+        </div>
+
+        <ManageData />
+     
+     <ListRender />
+
+     <ConditionalRender />
+    {/*props */}
+     <ShowUserName name = {userName}/>
+    {/*destructuring */}
+    <CarDetails brand= "VW" km= {10010} color= "red"/>
+
+    </div>
+  );
+}
+
+export default App;
