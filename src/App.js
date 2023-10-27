da1098330fb8bc467a98d4fdb97d50d6f7881add
Marcelo Biotto <lee.am@live.com>
2023-10-27 06:13:52 -0300

    alterando o componente cardetails e criando maps

--- src/App.js
+++ src/App.js
@@ -13,6 +13,33 @@ function App() {
 
   const name = "Joaquim";
   const[userName] = useState("Maria");
+
+
+  const cars=[
+    {
+      id:1,
+      brand: "Ferrari",
+      color:"Amarela",
+      newCar:true,
+      km:0
+    },
+    {
+      id:2,
+      brand: "Kia",
+      color:"Branco",
+      newCar:false,
+      km:34343
+    },
+    {
+      id:3,
+      brand: "Renault",
+      color:"Azul",
+      newCar:false,
+      km:234
+    }
+
+
+  ]
   return (
     <div className="App">
       <h1>
@@ -36,8 +63,18 @@ function App() {
     {/*props */}
      <ShowUserName name = {userName}/>
     {/*destructuring */}
-    <CarDetails brand= "VW" km= {10010} color= "red"/>
+    <CarDetails brand= "VW" km= {10010} color= "red" newCar={false}/>
+    {/* reaproveitando*/}
+    <CarDetails brand="Ford" color="Vermelha" km={0}  newCar={true}/>
 
+    <CarDetails brand="Fiat" color="Branca" km={4500} newCar={false}/>
+    {/*loop em array de objetos */}
+    {cars.map((car) => (
+      <CarDetails brand={car.brand} 
+      color={car.color} 
+      km={car.km} 
+      newCar={car.newCar}/>
+    ))}
     </div>
   );
 }
